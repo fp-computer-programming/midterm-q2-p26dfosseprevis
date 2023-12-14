73697dee9cc349643d75b8ea43b7e3d36a938f77
p26dfosseprevis <144148217+p26dfosseprevis@users.noreply.github.com>
2023-12-14 18:54:56 -0500

    Finishd playable verstion

--- Main.py
+++ Main.py
@@ -1,43 +1,48 @@
-"""
-Create a Python file named Midterm-Q2.py
-
-*** You must write a comment for every chunk of code you write from now on along with your author tag***
-
-"""
 import time
 import random
 import os
 from urllib.request import urlopen
-
+print("Hello Game master, this is dylans hangman simulator rtx 2000")
 #finds random word
-response = urlopen("https://www.mit.edu/~ecprice/wordlist.10000")
-txt = response.read()
-WORDS = txt.splitlines()
-word = str(random.choice(WORDS))
+word = ""
+while len(word) < 3:
+    """rolls random word"""
+    response = urlopen("https://www.mit.edu/~ecprice/wordlist.10000")
+    txt = response.read()
+    WORDS = txt.splitlines()
+    word = str(random.choice(WORDS))
 
-#cleans word for usege
-word = list(word)
-del(word[0])
-del(word[0])
-del(word[len(word)-1])
-word = "".join(word)
+    #cleans word for usege
+    word = list(word)
+    del(word[0])
+    del(word[0])
+    del(word[len(word)-1])
+    word = "".join(word)
 
 #code for the dude
 def dude(state):
+    """Writes dude to screen"""
     print (" /------")
     if state == 6:
         print (" ")
+        print (" ")
+        print (" ")
     elif state == 5:
         print (" O")
+        print (" ")
+        print (" ")
     elif state == 4:
         print (" O")
         print (" |")
+        print (" ")
     elif state == 3:
         print(" O")
         print("/|")
+        print (" ")
     elif state == 2:
         print(" O")
         print("/|\ ")
+        print (" ")
     elif state == 1:
         print(" O")
         print("/|\ ")
@@ -48,49 +53,47 @@ def dude(state):
         print(" /\ ")
                 
 def say(words):
-    time.sleep(2)
+    time.sleep(1)
     print(words)
 
-
-say("do you want our word or do you want to make a word?")
-setting = input("(our/own)")
+say("do you want our random word or do you want to make a word?")
+setting = input("(our/own): ")
 if setting == "our":
     say ("ok setting word")
-    say(f"the word is {word} I will now reload the log to for the user")
 elif setting == "own":
-    word = input("ok, what word do you want")
+    word = input("ok, what word do you want?: ")
 
 say(f"the word is {word} I will now reload the log to for the user")
-#os.system('cls')
+time.sleep(1)
+os.system('cls')
+say("hello player, wish you luck")
 state = 6 
 guess = []
 wl = list(word)
 for x in wl:
     guess += "_"
 attempt = []
-
-while wl != guess or state > 1:
+while "_" in guess and state >= 1:
     say("ok, so far the word looks like this")
     say(guess)
     say("and the dude looks like this")
     dude(state)
     say ("you have tried these letters") 
     say (attempt)
-    say("what is your guess")
+    say("what is your guess?: ")
     
     letter = input()[0]
     attempt += letter
     for x in range(len(wl)):
         if letter == wl[x]:
             guess[x] = letter
-
     if letter not in wl:
         state -= 1
-        say("opss sorry thats not in the word")
+        say("oops, sorry thats not in the word.")
 
-if state < 0:
+if state <= 0:
     dude(0)
-    say("sorry you failed")
+    say("you failed")
 else:
     say("good job you did it")
 
